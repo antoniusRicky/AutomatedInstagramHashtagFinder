a0e671722df909568959b4c560325239d37d2bf1
alvinp571 <putraalvin788@gmail.com>
2020-08-22 10:39:48 +0700

    debug retrieving hashtag error
    retrieving hashtag from caption

--- readFile.py
+++ readFile.py
@@ -1,13 +1,21 @@
 f = open("Testing.txt", "r")
 a = f.read()
 d = ""
-
+b = ""
 str = a.split()
 
 for y in str:
-    if y[0]=='#':
+    if y[0]=='#' :
         print(y)
         d += y
+    elif r"\n" in y:
+        if "#" in y:
+            b = y.split(r"\n")
+            for x in b:
+                if "#" in x:
+                    print(x)
+                    d += x
+
 
 #print(d)
 file1 = open("Testing2.txt", "w")
