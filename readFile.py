b49e1b7c7f302add91f68623518caef7ef79b0c8
alvinp571 <putraalvin788@gmail.com>
2020-08-22 10:59:17 +0700

    remove duplicate

--- readFile.py
+++ readFile.py
@@ -6,17 +6,21 @@ str = a.split()
 
 for y in str:
     if y[0]=='#' :
-        print(y)
+        #print(y)
         d += y
     elif r"\n" in y:
         if "#" in y:
             b = y.split(r"\n")
             for x in b:
                 if "#" in x:
-                    print(x)
+                    #print(x)
                     d += x
 
+my_str = d.split("#")
 
-#print(d)
+final_str = list(set(my_str))
+for z in final_str:
+    print(z)
+
 file1 = open("Testing2.txt", "w")
-file1.writelines(d)
+file1.writelines(final_str)
