789954fefd5a25d0b97103b9c6b96c13035c9958
pakapawz <63382223+pakapawz@users.noreply.github.com>
2020-08-26 12:49:21 +0700

    add gui

--- hashtag_search.py
+++ hashtag_search.py
@@ -59,65 +59,100 @@ def getRecentlySearchedHashtags(params):
 
     return makeApiCall(url, endpointParams, params['debug'])  # make the api call
 
-try:  # try and get param from command line
-    hashtag = sys.argv[1]  # hashtag to get info on
-except:  # default to coding hashtag
-    hashtag = 'chocolate'  # hashtag to get info on
+# try:  # try and get param from command line
+#     hashtag = sys.argv[1]  # hashtag to get info on
+# except:  # default to coding hashtag
+#     hashtag = 'chocolate'  # hashtag to get info on
+#
+# params = getCreds()  # params for api call
+# params['hashtag_name'] = hashtag  # add on the hashtag we want to search for
+# hashtagInfoResponse = getHashtagInfo(params)  # hit the api for some data!
+#
+# print("\n!!!HastagInfoResponse!!!")
+# print(hashtagInfoResponse['url'])
+# print(hashtagInfoResponse['endpoint_params_pretty'])
+# print(hashtagInfoResponse['json_data_pretty'])
+#
+# params['hashtag_id'] = hashtagInfoResponse['json_data']['data'][0]['id']; # store hashtag id
+#
+#
+# print ("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> HASHTAG INFO <<<<<<<<<<<<<<<<<<<<\n")  # section heading
+# print ("\nHashtag: " + hashtag)  # display hashtag
+# print ("Hashtag ID: " + params['hashtag_id'])  # display hashtag id
+#
+# print ("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> HASHTAG TOP MEDIA <<<<<<<<<<<<<<<<<<<<\n")  # section heading
+# params['type'] = 'top_media'  # set call to get top media for hashtag
+# hashtagTopMediaResponse = getHashtagMedia(params)  # hit the api for some data!
+#
+#
+# file1 = open("Testing.txt", "w")
+# for post in hashtagTopMediaResponse['json_data']['data']:  # loop over posts
+#     print ("\n\n---------- POST ----------\n")  # post heading
+#     print ("Link to post:")  # label
+#     print (post['permalink'])  # link to post
+#     print ("\nPost caption:")  # label
+#     print (post['caption'])  # post caption
+#     a = post['caption'].encode('unicode-escape').decode('ASCII')
+#     b = " "
+#     c = a+b
+#     file1.writelines(c)
+#     print ("\nMedia type:")  # label
+#     print (post['media_type'])  # type of media
+#
+# file1.close()
+#
+# print ("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> HASHTAG RECENT MEDIA <<<<<<<<<<<<<<<<<<<<\n")  # section heading
+# params['type'] = 'recent_media'  # set call to get recent media for hashtag
+# hashtagRecentMediaResponse = getHashtagMedia(params)  # hit the api for some data!
+#
+# for post in hashtagRecentMediaResponse['json_data']['data']:  # loop over posts
+#     print ("\n\n---------- POST ----------\n")  # post heading
+#     print ("Link to post:")  # label
+#     print (post['permalink'])  # link to post
+#     print ("\nPost caption:")  # label
+#     print (post['caption'])  # post caption
+#     print ("\nMedia type:")  # label
+#     print (post['media_type'])  # type of media
+#
+# print ("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> USERS RECENTLY SEARCHED HASHTAGS <<<<<<<<<<<<<<<<<<<<\n")  # section heading
+# getRecentSearchResponse = getRecentlySearchedHashtags(params)  # hit the api for some data!
+#
+# for hashtag in getRecentSearchResponse['json_data']['data']:  # looop over hashtags
+#     print ("\n\n---------- SEARCHED HASHTAG ----------\n")  # searched heading
+#     print ("\nHashtag: " + hashtag['name'])  # display hashtag
+#     print ("Hashtag ID: " + hashtag['id'])  # display hashtag id
 
-params = getCreds()  # params for api call
-params['hashtag_name'] = hashtag  # add on the hashtag we want to search for
-hashtagInfoResponse = getHashtagInfo(params)  # hit the api for some data!
-
-print("\n!!!HastagInfoResponse!!!")
-print(hashtagInfoResponse['url'])
-print(hashtagInfoResponse['endpoint_params_pretty'])
-print(hashtagInfoResponse['json_data_pretty'])
-
-params['hashtag_id'] = hashtagInfoResponse['json_data']['data'][0]['id']; # store hashtag id
 
 
-print ("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> HASHTAG INFO <<<<<<<<<<<<<<<<<<<<\n")  # section heading
-print ("\nHashtag: " + hashtag)  # display hashtag
-print ("Hashtag ID: " + params['hashtag_id'])  # display hashtag id
-
-print ("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> HASHTAG TOP MEDIA <<<<<<<<<<<<<<<<<<<<\n")  # section heading
-params['type'] = 'top_media'  # set call to get top media for hashtag
-hashtagTopMediaResponse = getHashtagMedia(params)  # hit the api for some data!
 
 
-file1 = open("Testing.txt", "w")
-for post in hashtagTopMediaResponse['json_data']['data']:  # loop over posts
-    print ("\n\n---------- POST ----------\n")  # post heading
-    print ("Link to post:")  # label
-    print (post['permalink'])  # link to post
-    print ("\nPost caption:")  # label
-    print (post['caption'])  # post caption
-    a = post['caption'].encode('unicode-escape').decode('ASCII')
-    b = " "
-    c = a+b
-    file1.writelines(c)
-    print ("\nMedia type:")  # label
-    print (post['media_type'])  # type of media
 
-file1.close()
+#INI FUNCTION COBA2
+def searchTopic(hashtag):
+    params = getCreds()
+    params['hashtag_name'] = hashtag
+    hashtagInfoResponse = getHashtagInfo(params)
 
-print ("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> HASHTAG RECENT MEDIA <<<<<<<<<<<<<<<<<<<<\n")  # section heading
-params['type'] = 'recent_media'  # set call to get recent media for hashtag
-hashtagRecentMediaResponse = getHashtagMedia(params)  # hit the api for some data!
+    params['hashtag_id'] = hashtagInfoResponse['json_data']['data'][0]['id'];  # store hashtag id
 
-for post in hashtagRecentMediaResponse['json_data']['data']:  # loop over posts
-    print ("\n\n---------- POST ----------\n")  # post heading
-    print ("Link to post:")  # label
-    print (post['permalink'])  # link to post
-    print ("\nPost caption:")  # label
-    print (post['caption'])  # post caption
-    print ("\nMedia type:")  # label
-    print (post['media_type'])  # type of media
+    print("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> HASHTAG INFO <<<<<<<<<<<<<<<<<<<<\n")  # section heading
+    print("\nHashtag: " + hashtag)  # display hashtag
+    print("Hashtag ID: " + params['hashtag_id'])  # display hashtag id
 
-print ("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> USERS RECENTLY SEARCHED HASHTAGS <<<<<<<<<<<<<<<<<<<<\n")  # section heading
-getRecentSearchResponse = getRecentlySearchedHashtags(params)  # hit the api for some data!
+    print("\n\n\n\t\t\t >>>>>>>>>>>>>>>>>>>> HASHTAG TOP MEDIA <<<<<<<<<<<<<<<<<<<<\n")  # section heading
+    params['type'] = 'top_media'  # set call to get top media for hashtag
+    hashtagTopMediaResponse = getHashtagMedia(params)  # hit the api for some data!
 
-for hashtag in getRecentSearchResponse['json_data']['data']:  # looop over hashtags
-    print ("\n\n---------- SEARCHED HASHTAG ----------\n")  # searched heading
-    print ("\nHashtag: " + hashtag['name'])  # display hashtag
-    print ("Hashtag ID: " + hashtag['id'])  # display hashtag id
+    file1 = open("Testing.txt", "w")
+    for post in hashtagTopMediaResponse['json_data']['data']:  # loop over posts
+        print("\n\n---------- POST ----------\n")  # post heading
+        print("Link to post:")  # label
+        print(post['permalink'])  # link to post
+        print("\nPost caption:")  # label
+        print(post['caption'])  # post caption
+        a = post['caption'].encode('unicode-escape').decode('ASCII')
+        b = " "
+        c = a + b
+        file1.writelines(c)
+        print("\nMedia type:")  # label
+        print(post['media_type'])  # type of media
